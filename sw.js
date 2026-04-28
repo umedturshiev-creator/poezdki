d24e1990c2b1f420b6e1d4b7107a61053c820adc
umedturshiev-creator <umedturshiev@gmail.com>
2026-04-28 11:02:20 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = 'smartpay-trips-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './smartpay-logo.png',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'smartpay-trips-v4';
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+      .catch(() => caches.match(event.request))
+  );
 });
